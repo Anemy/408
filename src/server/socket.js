954522dfc0e5a0177bade3c9bec1b144ddbab26c
Rhys Howell <rhysh@live.com>
2016-09-22 17:52:02 -0400

    Added a lobby manager, generating uuid for client on connected, tweaked movement speed

--- src/server/socket.js
+++ src/server/socket.js
@@ -5,11 +5,15 @@
  */
 
 const socketio = require('socket.io');
+const uuid = require('uuid');
+const LobbyManager = require('./lobbyManager');
 
 const SocketManager = function() {
   this.io = null;
 
   this.usersOnline = 0;
+
+  this.lobbyManager = new LobbyManager();
 
   this.startListening = function(server) {
     this.io = socketio(server);
@@ -18,21 +22,37 @@ const SocketManager = function() {
     this.io.sockets.on('connection', this.clientConnected.bind(this));
   };
 
+  // Handler for when a client connects to the server web socket.
   this.clientConnected = function(socket) {
     this.usersOnline++;
 
-    console.log('A client connected.');
+    // Assign the client a unique identifier.
+    socket.uuid = uuid.v4();
+    console.log('A client connected. Online:', this.usersOnline);
 
     // When a user disconnects from socket .
     socket.on('disconnect', function() {
-      this.usersOnline--;
-
       this.clientDisconnected();
     }.bind(this));
+
+    socket.on('message', function (data) {
+      this.messageRecieved(data);
+    }.bind(this));
+
+    // Notify the client that the server has recognized them, and pass them their unique id.
+    socket.emit('connected', {uuid: socket.uuid});
   };
 
+  // Handler for when a client disconnects.
   this.clientDisconnected = function() {
-    console.log('A client disconnected.');
+    this.usersOnline--;
+
+    console.log('A client disconnected. Online:', this.usersOnline);
+  };
+
+  // Handles socket messages 
+  this.messageRecieved = function() {
+
   };
 };
 
