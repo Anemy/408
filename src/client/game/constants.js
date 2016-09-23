01ae61d1160f2fb20a2244c81efdac31534223bc
Rhys Howell <rhysh@live.com>
2016-09-23 17:30:12 -0400

    Made player movement bounce on wall collisions, moved screen constants into file

---
+++ src/client/game/constants.js
@@ -0,0 +1,9 @@
+/**
+ * This file holds general purpose constants like the dimensions of the game.
+ */
+
+module.exports = {
+  // The user's browser dimensions.
+  width: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+  height: window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+};
--- src/client/game/draw/drawManager.js
+++ src/client/game/draw/drawManager.js
@@ -4,25 +4,22 @@
 
 // Load in the constants (colors, sizes, etc.) for drawing.
 const DrawConstants = require('./drawConstants');
+const Constants = require('../constants');
 
 class drawManager {
   initialize() {
-    // Get the user's browser dimensions.
-    this.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    this.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-
     const canvas = document.getElementById('gameCanvas');
 
     // The drawable canvas reference.
     this.ctx = canvas.getContext('2d');
 
-    this.ctx.canvas.width = this.width;
-    this.ctx.canvas.height = this.height;
+    this.ctx.canvas.width = Constants.width;
+    this.ctx.canvas.height = Constants.height;
   }
 
   draw(drawableObjects) {
     // Clear the last frame.
-    this.ctx.clearRect(0, 0, this.width, this.height); 
+    this.ctx.clearRect(0, 0, Constants.width, Constants.height); 
 
     this.drawMap();
 
@@ -34,9 +31,9 @@ class drawManager {
 
   drawMap() {
     this.ctx.fillStyle = DrawConstants.mapBackgroundColor;
-    this.ctx.fillRect(0,0, this.width, this.height);
+    this.ctx.fillRect(0,0, Constants.width, Constants.height);
 
-    const gridSize = this.width / DrawConstants.gridAmount;
+    const gridSize = Constants.width / DrawConstants.gridAmount;
     this.ctx.strokeStyle = DrawConstants.mapGridColor;
     for(var i = 0; i < DrawConstants.gridAmount + 1; i++) {
       // Randomly paint in certain squares to flicker.
@@ -54,21 +51,21 @@ class drawManager {
       // Vertical lines.
       this.ctx.beginPath();
       this.ctx.moveTo(gridSize * i, 0);
-      this.ctx.lineTo(gridSize * i, this.height);
+      this.ctx.lineTo(gridSize * i, Constants.height);
       this.ctx.stroke();
 
       // Horizontal lines.
       // Because of the 16:9 aspect ratio, only draw squares, this means that there are more in the square
-      if (gridSize * i < this.height) {
+      if (gridSize * i < Constants.height) {
         this.ctx.beginPath();
         this.ctx.moveTo(0, gridSize * i);
-        this.ctx.lineTo(this.width, gridSize * i);
+        this.ctx.lineTo(Constants.width, gridSize * i);
         this.ctx.stroke();
-      } else if(gridSize * (i - 1) <= this.height) {
+      } else if(gridSize * (i - 1) <= Constants.height) {
         // This draws the end border line.
         this.ctx.beginPath();
-        this.ctx.moveTo(0, this.height);
-        this.ctx.lineTo(this.width, this.height);
+        this.ctx.moveTo(0, Constants.height);
+        this.ctx.lineTo(Constants.width, Constants.height);
         this.ctx.stroke();
       }
     }
