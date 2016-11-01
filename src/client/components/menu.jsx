f7800d31ebb9599bcd7513814428ccfe3270b673
Jay Hankins <jay.hankins@outlook.com>
2016-11-01 16:20:02 -0400

    adds game instructions

--- src/client/components/menu.jsx
+++ src/client/components/menu.jsx
@@ -23,7 +23,34 @@ class Menu extends React.Component {
     } else if (this.props.navState === 'LOADING') {
       return <div>HASEKR</div>;
     } else {
-      return <div>ASLD</div>;
+      return <div className="instructions">
+        <ol>
+          <li>Open the game in your web browser.</li>
+          <li>The menu screen loads. Enter your name and select options. For example, you may choose a lobby to play in.</li>
+          <li>Click “Play Now” after selecting your options.</li>
+          <li>The game loads. You will see your player sprite on the screen. A 3 second countdown timer appears so you can get ready to play.</li>
+          <li>To move about the board:
+          <ul>
+          <li>The W key moves you up on the board.</li>
+          <li>The A key moves you left on the board.</li>
+          <li>The S key moves you down on the board.</li>
+          <li>The D key moves you right on the board.</li>
+          </ul>
+          </li>
+          <li>To shoot at other players:
+          <ul>
+          <li>The Up Arrow key shoots up.</li>
+          <li>The Left Arrow key shoots left.</li>
+          <li>The Down Arrow key shoots down.</li>
+          <li>The Right Arrow key shoots right.</li>
+          <li>You can also press two arrow keys at once. For example, Up Arrow and Right Arrow shoots at an upper right diagonal / northeast direction.</li>
+          </ul>
+          </li>
+          <li>The scoreboard is shown in the upper left corner of the screen.</li>
+          <li>Your health bar is rendered above your player sprite. The amount of green represents your health. When your health gets drained, the green is replaced with red.</li>
+          <li>Your score increases when you kill another player. </li>
+          </ol>
+      </div>;
     }
   }
 
